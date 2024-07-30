54358a0e302b350baa24141d5b54e1d0af5c756c
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-30 10:17:33 +0600

    Create queue_02_size.cpp

---
+++ queue_02_size.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    queue<int>q;
+    q.push(4);
+    q.push(5);
+    q.push(1);
+    q.push(6);
+    q.push(8);
+    q.push(4);
+    cout<<q.size()<<endl;
+}
